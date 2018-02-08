fb441e95204163168b580e05725745930cd1a2f6
vagrant <spencer.greff@gmail.com>
2018-02-08 19:39:16 +0000

    Added bootstrap and swiper functionality. Configured for mobile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,18 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+$(document).ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     }
-  });;
+  });
 });
