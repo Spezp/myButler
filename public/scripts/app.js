ab25b908547596f46db232410ad0578547970f32
vagrant <spencer.greff@gmail.com>
2018-02-09 03:52:50 +0000

    Created client side AJAX/Logic and slider partials

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false,
+    loop: true,
     
     // If we need pagination
     pagination: {
@@ -31,59 +31,38 @@ $(document).ready(function () {
       loadNewTodo();
     });
   });
+
+  const createTodoElement = function (todoDB) {
+
+    let template = `<tr><td>${todoDB.user.text}</td><td></td><td id="data-icon"><i class="fas fa-pencil-alt"></i><a ><i class="far fa-trash-alt"></i></a></td></tr>`;
+
+    return template;
+  };
+
   const renderTodos = (todos, newTodo) => {
     let contentAll = '';
 
     if (newTodo) {
-      $(createTodoElement(todos)).prependTo('.todo-container').hide().slideDown();
-      contentAll = todo.content.text; // append
-      $("#todo-string").text(contentAll);
+      $(createTodoElement(todos)).prependTo(`#${todos.user.category}`).hide().slideDown();
     } else {
       todos.forEach(function (todo) {
-        $(createTodoElement(todo)).prependTo('.todo-container');
-        contentAll = todo.content.text; // append
-        $("#todo-string").text(contentAll);
+        $(createTodoElement(todo)).prependTo(`#${todos.user.category}`);
       });
     }
-    contentAll = String(contentAll);
-    $("#todo-string").text(contentAll);
-  }
+  };
 
   let loadNewTodo = () => {
     $.getJSON("/user/:user_id/todo", (json) => {
       renderTodos(json[json.length - 1], true);
     });
   };
+
+  let loadTodos = () => {
+    $.getJSON("/user/:user_id/todo", (json) => {
+      renderTodos(json, false);
+    });
+  };
   
-  const createTodoElement = function (todoDB) {
-
-    let template = [
-
-      `<section class="tweet-article">`,
-
-      `<header>`,
-      `<img class="avatar" alt="${tweetDB.user.handle}" src="${tweetDB.user.avatars.small}">`,
-      `<h2>${tweetDB.user.name}</h2>`,
-      `<p class="handle">${tweetDB.user.handle}</p>`,
-      `</header>`,
-
-      `<article>`,
-      `<p id="tweet-string" ></p>`,
-      `</article>`,
-
-      `<footer>`,
-      `<p class="whenPosted">${datePosted(tweetDB.created_at)}</p>`,
-      `<div class="social">`,
-      `<a href="#"><i class="far fa-flag"></i></a>`,
-      `<a href="#"><i class="fas fa-retweet"></i></a>`,
-      `<a href="#"><i class="fas fa-heart"></i></a>`,
-      `</div">`,
-      `</footer>`,
-
-      `</section>`
-
-    ];
-
-    return template.join('');
-  }; */
+  loadTodos();
+  */
 });
